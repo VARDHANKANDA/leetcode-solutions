033655312086afd8c6bc0993963df6993aa242fc
Kanda Saptha Sri Vardhan <162998911+VARDHANKANDA@users.noreply.github.com>
2026-07-05 17:18:29 +0530

    Time: 36 ms (46.18%), Space: 44.2 MB (85.14%) - LeetHub

--- 0279-perfect-squares/0279-perfect-squares.java
+++ 0279-perfect-squares/0279-perfect-squares.java
@@ -3,7 +3,7 @@ class Solution {
         int[] dp = new int[n + 1];
         Arrays.fill(dp, Integer.MAX_VALUE);
         dp[0] = 0;
-        for (int i=0;i<=n;i++) {
+        for (int i=1;i<=n;i++) {
             for (int j=1;j*j<=i;j++) {
                 dp[i] = Math.min(dp[i],dp[i - j * j] + 1);
             }
